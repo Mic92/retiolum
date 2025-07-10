3f24c413eb3d94c99574af3fe6926c71675f8750
Jörg Thalheim <joerg@thalheim.io>
2025-07-10 13:21:19 +0200

    add darwin-nix configuration

---
+++ darwin/retiolum.nix
@@ -0,0 +1,135 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+
+with lib;
+
+let
+  netname = "retiolum";
+  cfg = config.networking.retiolum;
+  hosts = ../hosts;
+  genipv6 = import ../modules/retiolum/genipv6.nix { inherit lib; };
+in
+{
+  options = {
+    networking.retiolum.ipv4 = mkOption {
+      type = types.nullOr types.str;
+      default = null;
+      description = ''
+        own ipv4 address
+      '';
+    };
+    networking.retiolum.ipv6 = mkOption {
+      type = types.str;
+      default =
+        (genipv6 "retiolum" "external" {
+          hostName = cfg.nodename;
+        }).address;
+      description = ''
+        own ipv6 address
+      '';
+    };
+    networking.retiolum.nodename = mkOption {
+      type = types.str;
+      default = config.networking.hostName;
+      description = ''
+        tinc network name
+      '';
+    };
+    networking.retiolum.port = mkOption {
+      type = types.int;
+      default = 655;
+      description = ''
+        port tinc is listen
+      '';
+    };
+  };
+
+  config = {
+    services.tinc.networks.${netname} = {
+      name = cfg.nodename;
+      extraConfig = ''
+        Port = ${toString cfg.port}
+        LocalDiscovery = yes
+
+        ConnectTo = eva
+        ConnectTo = eve
+        ConnectTo = ni
+        ConnectTo = prism
+        AutoConnect = yes
+      '';
+    };
+
+    environment.etc."hosts".text =
+      if (cfg.ipv4 == null) then
+        builtins.readFile ../etc.hosts-v6only
+      else
+        builtins.readFile ../etc.hosts;
+
+    environment.systemPackages = [
+      config.services.tinc.networks.${netname}.package
+    ];
+
+    # Darwin-specific implementation for installing host keys
+    launchd.daemons."tinc.${netname}-host-keys" =
+      let
+        install-keys = pkgs.writeShellScript "install-keys" ''
+          rm -rf /etc/tinc/${netname}/hosts.tmp
+          mkdir -p /etc/tinc/${netname}/hosts.tmp
+          cp -R ${hosts}/* /etc/tinc/${netname}/hosts.tmp
+          chmod -R u+w /etc/tinc/${netname}/hosts.tmp
+
+          rm -rf /etc/tinc/${netname}/hosts
+          mv /etc/tinc/${netname}/hosts.tmp /etc/tinc/${netname}/hosts
+        '';
+      in
+      {
+        command = toString install-keys;
+        serviceConfig = {
+          Label = "org.tinc-vpn.${netname}.host-keys";
+          RunAtLoad = true;
+          StandardErrorPath = "/var/log/tinc.${netname}-host-keys.log";
+          StandardOutPath = "/var/log/tinc.${netname}-host-keys.log";
+        };
+      };
+
+    # Darwin doesn't have systemd-networkd, so we need to configure the network interface differently
+    # This will need to be done via launchd and ifconfig
+    launchd.daemons."tinc.${netname}-network" = {
+      command = toString (
+        pkgs.writeShellScript "tinc-network-setup" ''
+          # Wait for the tinc interface to come up
+          while ! ifconfig tinc.${netname} >/dev/null 2>&1; do
+            sleep 1
+          done
+
+          # Configure the interface
+          ${optionalString (cfg.ipv4 != null) ''
+            ifconfig tinc.${netname} inet ${cfg.ipv4} netmask 255.240.0.0
+          ''}
+          ${optionalString (cfg.ipv6 != null) ''
+            ifconfig tinc.${netname} inet6 ${cfg.ipv6} prefixlen 16
+          ''}
+
+          # Set MTU
+          ifconfig tinc.${netname} mtu 1377
+        ''
+      );
+
+      serviceConfig = {
+        Label = "org.tinc-vpn.${netname}.network";
+        RunAtLoad = true;
+        KeepAlive = false;
+        StandardErrorPath = "/var/log/tinc.${netname}-network.log";
+        StandardOutPath = "/var/log/tinc.${netname}-network.log";
+      };
+    };
+
+    warnings = lib.optional (cfg.ipv6 == null) ''
+      `networking.retiolum.ipv6` is not set
+    '';
+  };
+}
--- flake.nix
+++ flake.nix
@@ -1,8 +1,82 @@
 {
   description = "Nix flake for retiolum VPN";
 
-  outputs = { self }: {
-    nixosModules.retiolum = import ./modules/retiolum;
-    nixosModules.ca = import ./modules/ca;
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      darwin,
+    }:
+    {
+      nixosModules.retiolum = ./modules/retiolum;
+      nixosModules.ca = ./modules/ca;
+      darwinModules.tinc = ./darwin/tinc.nix;
+      darwinModules.retiolum = ./darwin/retiolum.nix;
+
+      # Example Darwin configuration for testing
+      darwinConfigurations.example = darwin.lib.darwinSystem {
+        system = "aarch64-darwin";
+        modules = [
+          self.darwinModules.tinc
+          self.darwinModules.retiolum
+          (
+            { pkgs, ... }:
+            {
+              # Basic nix-darwin configuration
+              nix.settings.experimental-features = [
+                "nix-command"
+                "flakes"
+              ];
+
+              # Enable retiolum
+              networking.retiolum = {
+                nodename = "example";
+                ipv4 = "10.243.99.99";
+                ipv6 = "42:0:3c46:dead:beef:dead:beef:dead";
+              };
+
+              # Required for nix-darwin
+              system.stateVersion = 4;
+            }
+          )
+        ];
+      };
+
+      # Example NixOS configuration for testing
+      nixosConfigurations.example = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          self.nixosModules.retiolum
+          (
+            { pkgs, ... }:
+            {
+              # Basic NixOS configuration
+              boot.loader.grub.enable = false;
+              fileSystems."/" = {
+                device = "tmpfs";
+                fsType = "tmpfs";
+              };
+
+              # Enable retiolum
+              networking.retiolum = {
+                nodename = "example";
+                ipv4 = "10.243.99.99";
+                ipv6 = "42:0:3c46:dead:beef:dead:beef:dead";
+              };
+
+              # Required for NixOS
+              system.stateVersion = "23.11";
+            }
+          )
+        ];
+      };
+    };
 }
